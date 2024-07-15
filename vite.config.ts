e1c84ef2ded5c99f0ead34ddbc7e2c8aa0e56413
Rohan bohre <rohans540@gmail.com>
2024-07-15 18:04:22 +0530

    setup inial app and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsConfigPaths(), react()],
+  server:{
+    port: 3000,
+    host: 'localhost'
+  }
 });
